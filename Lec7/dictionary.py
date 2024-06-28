76b5aa9eb92da573b23befcca437c22d9f105700
MichaelYeQian <michael.2103ll@gmail.com>
2024-06-28 11:25:01 -0700

    Dictionary study notes

---
+++ Lec7/dictionary.py
@@ -0,0 +1,21 @@
+#chuangjian dictionary
+prog_dictionary = {
+    "Bug" : "program error" , 
+    "loop" :"do something iteratively"
+}
+
+#calling
+print(prog_dictionary["Bug"]) 
+
+#modification
+prog_dictionary["Bug"] = "cuowu"
+print(prog_dictionary["Bug"])
+
+#add
+prog_dictionary["function"] = "y=kx"
+print(prog_dictionary)
+
+
+
+
+
