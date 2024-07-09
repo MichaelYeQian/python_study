27250516c5d086593644b735905bd3eb07470b72
MichaelYeQian <michael.2103ll@gmail.com>
2024-07-08 18:03:04 -0700

    local and global varible

---
+++ Lec9/local_global_varible.py
@@ -0,0 +1,44 @@
+enemies = 1
+def increase_enemies():
+    enemies = 2
+    test = 3
+    print(f"enemies inside function:{enemies}")
+
+increase_enemies()
+print(f"enemies outside function:{enemies}")
+
+#print(test)  域里面是local variable，外面无法打印
+#--------------------------------------------------
+#作用域叫scopy
+#global scope
+player_health = 10#global varible
+
+def drink_potion():
+    potion_strength = 2
+    print(player_health)
+
+drink_potion()
+print(player_health)
+
+#------------------------------------------------------
+enemies = 1
+
+def increase_enemies():
+    print(f"enemies inside function:{enemies}")
+    return enemies + 1
+    
+
+increase_enemies()
+print(f"enemies outside function:{enemies}")
+
+
+def increase_enemies():
+    global enemies
+    enemies += 1
+    print(f"enemies inside function:{enemies}")
+
+increase_enemies()
+print(f"enemies outside function:{enemies}")
+
+
+
